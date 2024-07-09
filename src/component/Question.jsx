fe6266214da1e69ba3834f55992fc6058b76e3bb
Ayush-kathayat <kathayatayush789@gmail.com>
2024-07-09 07:58:33 +0530

    Added the Question component which will have the question and its option

---
+++ src/component/Question.jsx
@@ -0,0 +1,23 @@
+
+import './Question.css';
+
+const Question = ({ question, options, handleAnswer, selectedOption }) => {
+  return (
+    <div className="question-container">
+      <h2>{question}</h2>
+      <div className="options-container">
+        {options.map((option, index) => (
+          <button
+            key={index}
+            className={`option-button ${selectedOption === option ? "selected" : ""}`}
+            onClick={() => handleAnswer(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Question;
